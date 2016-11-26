e0087d4cd6aea8e303b9da1200eb47bd5e83ae75
Sherlock Wang <wangzhou.bry@gmail.com>
2016-11-26 20:43:30 +0800

    ...
    Signed-off-by: Sherlock Wang <wangzhou.bry@gmail.com>

--- .scull.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/sherlock/tests/ldd3/scull/scull.ko := aarch64-linux-gnu-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/sherlock/tests/ldd3/scull/scull.ko /home/sherlock/tests/ldd3/scull/scull.o /home/sherlock/tests/ldd3/scull/scull.mod.o ;  true
--- .tmp_versions/scull.mod
+++
@@ -1,3 +0,0 @@
-/home/sherlock/tests/ldd3/scull/scull.ko
-/home/sherlock/tests/ldd3/scull/scull.o
-
